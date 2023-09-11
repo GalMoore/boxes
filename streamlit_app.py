4f0dc813a0f863e68214f45b04f88f0a4b8e59b0
Gal Moore <gal@galmoore.co.uk>
2023-09-11 13:43:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,21 @@
+import streamlit as st
+from graphviz import Digraph
+
+# Create a Digraph object
+dot = Digraph(comment="Four Boxes with Arrows")
+
+# Add nodes (boxes)
+dot.node('A', 'Box A')
+dot.node('B', 'Box B')
+dot.node('C', 'Box C')
+dot.node('D', 'Box D')
+
+# Add edges (arrows)
+dot.edge('A', 'B')
+dot.edge('B', 'C')
+dot.edge('C', 'D')
+dot.edge('D', 'A')
+
+# Show the diagram in Streamlit
+st.graphviz_chart(dot.source)
 
