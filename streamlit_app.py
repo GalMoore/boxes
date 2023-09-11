397dc655f28dadd75246ad8c5e06872962110b23
Gal Moore <gal@galmoore.co.uk>
2023-09-11 13:48:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,29 @@
 import streamlit as st
 from graphviz import Digraph
 
-col1, col2 = st.columns(2)
-col1.write("This is column 1")
-col2.write("This is column 2")
+col1, col2,col3,col4,col5,col6 = st.columns(2)
+col1.write("This is column 1 and here is some details about it")
+col2.write("This is column 2 and here is some details about it")
+col3.write("This is column 3 and here is some details about it")
+col4.write("This is column 4 and here is some details about it")
+col5.write("This is column 5 and here is some details about it")
+col6.write("This is column 6 and here is some details about it")
                
-# Create a Digraph object
-dot = Digraph(comment="Four Boxes with Arrows")
+# # Create a Digraph object
+# dot = Digraph(comment="Four Boxes with Arrows")
 
-# Add nodes (boxes)
-dot.node('A', 'Box A')
-dot.node('B', 'Box B')
-dot.node('C', 'Box C')
-dot.node('D', 'Box D')
+# # Add nodes (boxes)
+# dot.node('A', 'Box A')
+# dot.node('B', 'Box B')
+# dot.node('C', 'Box C')
+# dot.node('D', 'Box D')
 
-# Add edges (arrows)
-dot.edge('A', 'B')
-dot.edge('B', 'C')
-dot.edge('C', 'D')
-dot.edge('D', 'A')
+# # Add edges (arrows)
+# dot.edge('A', 'B')
+# dot.edge('B', 'C')
+# dot.edge('C', 'D')
+# dot.edge('D', 'A')
 
-# Show the diagram in Streamlit
-st.graphviz_chart(dot.source)
+# # Show the diagram in Streamlit
+# st.graphviz_chart(dot.source)
 
