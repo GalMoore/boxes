323df13dc49cc22afc7fd6119dbe0699f9cd24d1
Gal Moore <gal@galmoore.co.uk>
2023-09-11 13:50:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 from graphviz import Digraph
 
 col1, col2,col3,col4,col5,col6 = st.columns(6)
-col1.write("This is column 1 and here is some details about it")
-col2.write("This is column 2 and here is some details about it")
-col3.write("This is column 3 and here is some details about it")
-col4.write("This is column 4 and here is some details about it")
-col5.write("This is column 5 and here is some details about it")
-col6.write("This is column 6 and here is some details about it")
+col1.write("This is column 1 and here is some details about it HERE IS THE DATE 35/5325/5 and some presecription: do this and that ")
+col2.write("This is column 2 and here is some details about it HERE IS THE DATE 35/5325/5 and some presecription: do this and that")
+col3.write("This is column 3 and here is some details about it HERE IS THE DATE 35/5325/5 and some presecription: do this and that")
+col4.write("This is column 4 and here is some details about it HERE IS THE DATE 35/5325/5 and some presecription: do this and that")
+col5.write("This is column 5 and here is some details about it HERE IS THE DATE 35/5325/5 and some presecription: do this and that")
+col6.write("This is column 6 and here is some details about it HERE IS THE DATE 35/5325/5 and some presecription: do this and that")
                
 # # Create a Digraph object
 # dot = Digraph(comment="Four Boxes with Arrows")
